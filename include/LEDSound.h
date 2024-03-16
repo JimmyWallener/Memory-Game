eb5eb6d3442b22b2db254aa694d6ccd828d7a6d2
livDan <liv.danielsdottir@chasacademy.se>
2024-03-16 10:18:52 +0100

    lagt till några kommentarer samt ändrat namn på variabler

--- include/LEDSound.h
+++ include/LEDSound.h
@@ -1,33 +1,34 @@
+//En funktion för att spela upp ljud för lamporna. 
+//funktionen tar lamp-nummer samt tid som ljudet ska låta. 
+//Lamporna är numrerade i samma ordning som i ButtonIndex i button.cpp. 
+
+
 #ifndef LED_SOUND
 #define LED_SOUND
+
 #ifdef __AVR__
-
-
 #include <avr/power.h>
 #endif
 #include <Arduino.h>
-#define R_LED 262
-#define Y_LED 294
-#define B_LED 330
-#define G_LED 349
+#define R_LED 262 //(C)
+#define Y_LED 294 //(D)
+#define B_LED 330 //(E)
+#define G_LED 349 //(F)
 
 void playLED(int LED, int time) //index för lampa, tid för lampa att lysa (kanske delaytime?)
 {
 
-// change this to whichever pin you want to use
-int buzzerN = 11;
+//Pin för summer
+int buzzer = 11;
 
-int melodyN[] = {
+int LEDSound[] = {
 R_LED, Y_LED, B_LED,  G_LED, 
 };
 
- tone(buzzerN, melodyN[LED-1], time);
+ tone(buzzer, LEDSound[LED-1], time);
 
 // Behöver vi delay här?
 delay(time);
-
-
-
 
 }
 
